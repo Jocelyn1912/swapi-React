b6840b9eab5a26b0718eeed773da707fa36f9ada
Jocelyn1912 <toty_2410@hotmail.com>
2018-03-15 23:11:48 -0300

    Agregando más personajes

--- src/componentes/Personajes/Personajes.js
+++ src/componentes/Personajes/Personajes.js
@@ -1,7 +1,18 @@
 import React, {Component} from 'react';
-import img from './../../img/luke.jpg';
 import './Personajes.css';
+import img1 from './../../img/luke.jpg';
 import img2 from './../../img/c3po.jpg';
+import img3 from './../../img/r2d2.jpg';
+import img4 from './../../img/darth-vader.jpg';
+import img5 from './../../img/leia.jpg';
+import img10 from './../../img/obi.jpg';
+import img13 from './../../img/chewacca.jpg';
+import img14 from './../../img/solo.png';
+import img19 from './../../img/yoda.jpg';
+import img21 from './../../img/boba-fett.jpg';
+import img67 from './../../img/sith.jpg';
+import img69 from './../../img/stormtrooper.jpg';
+
 
 class Personajes extends Component {
   render() {
@@ -9,10 +20,40 @@ class Personajes extends Component {
       <div>
 
        <div className="col-lg-3 col-md-3 col-sm-6 col-xs-6">
-          <button className="personaje" value={1} type="button"><img src={img} className="img-responsive"/></button>
+          <button className="personaje" value={1} type="button"><img src={img1} className="img-responsive"/></button>
        </div>
        <div className="col-lg-3 col-md-3 col-sm-6 col-xs-6">
           <button className="personaje" value={2} type="button"><img src={img2} className="img-responsive"/></button>
+       </div>
+       <div className="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <button className="personaje" value={3} type="button"><img src={img3} className="img-responsive"/></button>
+       </div>
+       <div className="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <button className="personaje" value={4} type="button"><img src={img4} className="img-responsive"/></button>
+       </div>
+       <div className="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <button className="personaje" value={5} type="button"><img src={img5} className="img-responsive"/></button>
+       </div>
+       <div className="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <button className="personaje" value={10} type="button"><img src={img10} className="img-responsive"/></button>
+       </div>
+       <div className="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <button className="personaje" value={13} type="button"><img src={img13} className="img-responsive"/></button>
+       </div>
+       <div className="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <button className="personaje" value={14} type="button"><img src={img14} className="img-responsive"/></button>
+       </div>
+       <div className="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <button className="personaje" value={19} type="button"><img src={img19} className="img-responsive"/></button>
+       </div>
+       <div className="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <button className="personaje" value={21} type="button"><img src={img21} className="img-responsive"/></button>
+       </div>
+       <div className="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <button className="personaje" value={67} type="button"><img src={img67} className="img-responsive"/></button>
+       </div>
+       <div className="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <button className="personaje" value={69} type="button"><img src={img69} className="img-responsive"/></button>
        </div>
        
      </div>
