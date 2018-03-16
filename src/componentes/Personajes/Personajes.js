abcc24e6900e9c29b9fb62da23b116b51b02c88b
pamearteaga <pe_arteaga@hotmail.com>
2018-03-15 22:53:33 -0300

    personajes

--- src/componentes/Personajes/Personajes.js
+++ src/componentes/Personajes/Personajes.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
 import './Personajes.css';
 import img from './../../img/luke.jpg';
+import img2 from './../../img/c3po.jpg';
 
 
 class Personajes extends Component {
   render() {
     return (
-     <div className="col-lg-3 col-md-3 col-sm-6 col-xs-6">
-        <button className="personaje" value={1} type="button"><img src={img} className="img-responsive"/></button>
+      <div>
+
+       <div className="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <button className="personaje" value={1} type="button"><img src={img} className="img-responsive"/></button>
+       </div>
+       <div className="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <button className="personaje" value={2} type="button"><img src={img2} className="img-responsive"/></button>
+       </div>
+       
      </div>
     )
   }
