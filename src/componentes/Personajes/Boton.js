8217cace35b6e924dc4e6137cf32e4d2f3c8ce97
pamearteaga <pe_arteaga@hotmail.com>
2018-03-15 22:16:07 -0300

    componente personajes

---
+++ src/componentes/Personajes/Boton.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+//import Personajes from './Personajes.js';
+import datos from './../../personajes.json';
+
+class Boton extends Component {
+  render() {
+    return(
+      <div>
+        {
+          datos.map(function(item) {
+            return 
+            console.log(item.personajes);
+            
+          })
+        }
+      </div>
+    )
+  }
+}
+
+export default Boton;
